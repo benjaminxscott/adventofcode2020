f34dfcaed50936cc4a62212271458820a8dd2fc8
Benjamin Scott <1711810+benjaminxscott@users.noreply.github.com>
2020-12-20 12:24:58 -0800

    add 8

---
+++ 8/day8_2.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2020/day/8
+
+'''
+After some careful analysis, you believe that exactly one instruction is corrupted.
+
+Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp. (No acc instructions were harmed in the corruption of this boot code.)
+
+The program is supposed to terminate by attempting to execute an instruction immediately after the last instruction in the file.
+By changing exactly one jmp or nop, you can repair the boot code and make it terminate correctly.
+
+For example, consider the same program from above:
+
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction infinite loop, never leaving that instruction.
+If you change almost any of the jmp instructions, the program will still eventually find another jmp instruction and loop forever.
+
+However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program terminates! The instructions are visited in this order:
+
+nop +0  | 1
+acc +1  | 2
+jmp +4  | 3
+acc +3  |
+jmp -3  |
+acc -99 |
+acc +1  | 4
+nop -4  | 5
+acc +6  | 6
+After the last instruction (acc +6), the program terminates by attempting to run the instruction below the last instruction in the file.
+With this change, after the program terminates, the accumulator contains the value 8 (acc +1, acc +1, acc +6).
+
+Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp). What is the value of the accumulator after the program terminates?
+'''
+
+
+def main():
+    with open ('input.txt') as fp:
+        lines = fp.read().splitlines()
+
+        acc = 0
+
+        eip = 0 # index into lines of instructions
+        visited_instructions = []
+
+        i = 0
+        # break if an instruction is about to be executed twice
+        while eip not in visited_instructions:
+
+            instruction, offset = lines[eip].split()
+            offset = int(offset)
+            visited_instructions.append(eip)
+            i+=1
+            print(f"{i}:{instruction} with {offset}")
+
+            if instruction == 'acc':
+                acc += offset
+                eip += 1
+            elif instruction == 'nop':
+                eip += 1
+            elif instruction == 'jmp':
+                eip += offset
+
+        # s.b. '5' on test input
+        print(f"accumulator: {acc}")
+
+if __name__ == '__main__':
+    main()
